963dc4bc35745306fc34676fccee8f60b1acf780
Ana Karina Chiriff Graces <akchiriff@gmail.com>
2026-03-16 12:03:59 +0100

    Add newline at the end of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,3 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // ⚠️ Cambia 'planning-poker-viva' por el nombre exacto de tu repo en GitHub
 export default defineConfig({ plugins: [react()], base: '/planning-poker-viva/' })
+
